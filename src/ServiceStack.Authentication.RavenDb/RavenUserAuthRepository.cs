61c055620869c7682884a18c9bf80f09c2e3ff94
Adam Larsen <adam@collabip.com>
2013-05-20 12:04:08 -0500

    Fix for GetUserAuth from RavenDB.
    Converted the UserAuthId to an Int so Raven can load the document by ID.

--- src/ServiceStack.Authentication.RavenDb/RavenUserAuthRepository.cs
+++ src/ServiceStack.Authentication.RavenDb/RavenUserAuthRepository.cs
@@ -202,7 +202,10 @@ namespace ServiceStack.Authentication.RavenDb
 		{
 			using (var session = _documentStore.OpenSession())
 			{
-				return session.Load<UserAuth>(userAuthId);
+                int authId;
+                if (int.TryParse(userAuthId, out authId))
+                    return session.Load<UserAuth>(authId); 
+                return session.Load<UserAuth>(userAuthId);
 			}
 		}
 
