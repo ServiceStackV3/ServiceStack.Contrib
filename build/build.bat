4ee30428b843660a148ad3bef704ac7128ec3a00
Demis Bellot <demis.bellot@gmail.com>
2013-09-14 15:50:26 -0400

    Make build files consistent across all projs

--- build/build.bat
+++ build/build.bat
@@ -1,11 +1,3 @@
-REM SET BUILD=Debug
-SET BUILD=Release
+SET MSBUILD=C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe
 
-COPY ..\src\ServiceStack.Authentication.MongoDB\bin\%BUILD%\ServiceStack.Authentication.MongoDB.* ..\NuGet\ServiceStack.Authentication.MongoDB\lib
-COPY ..\src\ServiceStack.Authentication.RavenDB\bin\%BUILD%\ServiceStack.Authentication.RavenDB.* ..\NuGet\ServiceStack.Authentication.RavenDB\lib
-COPY ..\src\ServiceStack.Authentication.NHibernate\bin\%BUILD%\ServiceStack.Authentication.NHibernate.* ..\NuGet\ServiceStack.Authentication.NHibernate\lib
-COPY ..\src\ServiceStack.CacheAccess.Azure\bin\%BUILD%\ServiceStack.CacheAccess.Azure.* ..\NuGet\ServiceStack.Caching.Azure\lib
-COPY ..\src\ServiceStack.CacheAccess.AwsDynamoDb\bin\%BUILD%\ServiceStack.CacheAccess.AwsDynamoDb.* ..\NuGet\ServiceStack.Caching.AwsDynamoDb\lib
-COPY ..\src\ServiceStack.CacheAccess.Memcached\bin\%BUILD%\ServiceStack.CacheAccess.Memcached.* ..\NuGet\ServiceStack.Caching.Memcached\lib
-
-REM Using EnyimMemcached from NuGet: COPY ..\src\ServiceStack.CacheAccess.Memcached\bin\%BUILD%\Enyim.Caching.* ..\NuGet\ServiceStack.Caching.Memcached\lib
+%MSBUILD% Build.proj /target:NuGetPack /property:Configuration=Release;RELEASE=true;PatchVersion=60
