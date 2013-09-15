fa86e271da9b26381661178792bb35e88eed2553
Demis Bellot <demis.bellot@gmail.com>
2013-09-15 16:40:39 -0400

    Keep consistent NuGet structure, update NuGet.exe

---
+++ NuGet/NuGetPush.cmd
@@ -0,0 +1,13 @@
+SET NUGET=..\src\.nuget\nuget
+%NUGET% push ServiceStack.Authentication.MongoDB.3.9.60.nupkg
+%NUGET% push ServiceStack.Authentication.MongoDB.3.9.60.symbols.nupkg
+%NUGET% push ServiceStack.Authentication.NHibernate.3.9.60.nupkg
+%NUGET% push ServiceStack.Authentication.NHibernate.3.9.60.symbols.nupkg
+%NUGET% push ServiceStack.Authentication.RavenDB.3.9.60.nupkg
+%NUGET% push ServiceStack.Authentication.RavenDB.3.9.60.symbols.nupkg
+%NUGET% push ServiceStack.Caching.AwsDynamoDb.3.9.60.nupkg
+%NUGET% push ServiceStack.Caching.AwsDynamoDb.3.9.60.symbols.nupkg
+%NUGET% push ServiceStack.Caching.Azure.3.9.60.nupkg
+%NUGET% push ServiceStack.Caching.Azure.3.9.60.symbols.nupkg
+%NUGET% push ServiceStack.Caching.Memcached.3.9.60.nupkg
+%NUGET% push ServiceStack.Caching.Memcached.3.9.60.symbols.nupkg
